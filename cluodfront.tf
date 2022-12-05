0f169342434d2c9b0fe21d67fc6ba83382a85b8b
sanu1020 <sanuuthaya02@gmail.com>
2022-12-05 12:35:11 +0530

    github workflow added

--- cluodfront.tf
+++ cluodfront.tf
@@ -22,6 +22,25 @@ resource "aws_cloudfront_distribution" "s3_distribution_sanujan_2" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
+  
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
   ordered_cache_behavior {
     path_pattern     = "/index.html"
